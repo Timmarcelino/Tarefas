e33cd222f8d3aeea6050cd18352bd513db662cee
Timmarcelino <valentim.marcelino@live.com>
2023-08-28 19:17:12 -0300

    Tarefa 8.2 1 start

---
+++ Fase1Nv2/README.md
@@ -0,0 +1,12 @@
+ Algoritmos e Problemas
+
+Tipos:
+    String
+        Missing Character, básico (missingCharacter.rb)        
+        Count, fácil
+    Array
+        Find Three, fácil
+        Reversal, fácil
+    Date
+        Find Future, fácil
+        Calculate Age, fácil
---
+++ Fase1Nv2/missingCharacter.rb
@@ -0,0 +1,8 @@
+alfabeto = ('a'..'z').to_a
+print 'Digite uma frase: '
+frase = gets.chomp
+letras_presentes = frase.downcase.scan(/[a-z]/).uniq
+letras_faltantes = alfabeto - letras_presentes
+
+puts " A Frase digitada é: #{frase}!\n"
+letras_faltantes.empty? ? puts "A frase é um pangrama!" : puts " Faltam as letras: #{letras_faltantes.join(', ')}"
