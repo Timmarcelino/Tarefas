2b59f9cb28f7a604cc8d6862ddec9d975bc7a5cf
Timmarcelino <valentim.marcelino@live.com>
2023-08-28 21:38:48 -0300

    Tarefa 8.2 2 solucionado

--- Fase1Nv2/count.rb
+++ Fase1Nv2/count.rb
@@ -1,4 +1,21 @@
-input = '#GeeKs01fOr@gEEks07'
-for e in input.chars do
-  puts "#{e.ord}\n"
+input = '#GeeKs01 fOr@gEEks07'
+upper = 0
+lower = 0
+number = 0
+special = 0
+
+if input.length > 0
+  for e in input.chars do
+    if e.ord >= 65 and e.ord <=90
+      upper += 1
+    elsif e.ord >= 97 and e.ord <=122
+      lower += 1
+    elsif e.ord >= 48 and e.ord <=57
+      number += 1
+    else
+      special += 1
+    end
+  end
 end
+
+puts "\n Maiúsculas =  #{upper}\n Minúsculas = #{lower}\n números = #{number}\n caracteres especiais = #{special}\n"
