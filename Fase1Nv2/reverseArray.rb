60781fef30ba8200719994afb1e33f5b10f26ca3
Timmarcelino <valentim.marcelino@live.com>
2023-08-31 01:33:10 -0300

    Solução

--- Fase1Nv2/reverseArray.rb
+++ Fase1Nv2/reverseArray.rb
@@ -1,21 +1,13 @@
-def reverseArray(array, k)
-  indiceMaior = array.length - 1
-  indiceMenor = 0
-  qteOutrosInd = array.length - k
-  while k >= 0 do
-    elementA = array[indiceMenor]
-    elementB = array[indiceMaior]
-    resultado =
-    k = k-1
+def rotateArray(array, k)
+  n = array.length
+  if k < n && k > 0
+    element = array.slice!(-k, k) # .slice! : Remove e retorna os elementos removidos
+    return  element + array       # O operador + retorna um novo array concatenado (união) dos dois arrays
+  else
+    return array
   end
-
-
-
-
-
-
 end
-
 input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 k = 3
-puts reverseArray(input, k)
+puts input.inspect # .inspect: Retorna a nova String formada pela chamada do método
+puts rotateArray(input, k).inspect
