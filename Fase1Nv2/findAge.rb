abbcf888a08fe5f0db93fc57287133ce0b72d22b
Timmarcelino <valentim.marcelino@live.com>
2023-09-01 16:03:34 -0300

    Nova tarefa

---
+++ Fase1Nv2/findAge.rb
@@ -0,0 +1,18 @@
+def findAge(dataNasc, data)
+
+end
+
+#Entrada de dados
+
+diaN = 23
+mesN = 01
+anoN = 1981
+
+diaI = 30
+mesI = 01
+anoI = 2040
+
+dataNascimento = diaN + '/' + mesN + '/' + anoN
+dataInserida = diaI + '/' + mesI + '/' + anoI
+
+findAge(dataNascimento, dataInserida)
