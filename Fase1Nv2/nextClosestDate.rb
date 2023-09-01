91e8303e2176cfd56cc708be0b6a0a04a817626d
Timmarcelino <valentim.marcelino@live.com>
2023-09-01 12:34:16 -0300

    Inicio Data Futura mais proxima

---
+++ Fase1Nv2/nextClosestDate.rb
@@ -0,0 +1,10 @@
+### Encontrar a data futura mais próxima
+## Primeiro Ordenar in reverso.
+## Comparar cada query para localizar a data futura mais proxima
+#Date.strptime('03-02-2001', '%d-%m-%Y')
+
+
+
+
+arr = [“22/4/1233”, “1/3/633”, “23/5/56645”, “4/12/233”]
+query = [“23/3/4345”, “12/3/2”]
