a7aa0e882fcdcecd5e29d687882e6ce59f9b951f
Timmarcelino <valentim.marcelino@live.com>
2023-09-02 13:20:58 -0300

    Sabadando

--- Fase1Nv2/findAge.rb
+++ Fase1Nv2/findAge.rb
@@ -4,32 +4,45 @@ def findAge(dataNasc, data)
   formatDate = "%d/%m/%Y"
   nasc = Date.strptime(dataNasc, formatDate)
   dataInformada = Date.strptime(data, formatDate)
-  hoje = Date.today
+  #hoje = Date.today
 
-  #age1 = dataInformada - nasc
-  age1 = hoje - nasc
 
-  anos = age1.to_i / 365
-  meses = (age1.to_i % 365 ) /12
+  anos = (dataInformada - nasc).to_i / 365
 
-  puts age1.to_i
-  puts age1.to_i / 365
-  puts  (age1.to_i % 365)
-  puts (age1.to_i % 365) / 30
+  #Se o dia já tiver passado então é o hoje - dia do nascimento
+  if nasc.mday > dataInformada.mday
+    dias = (dataInformada - Date.new((dataInformada -31).year, (dataInformada-31).mon, nasc.mday)).to_i
+    if nasc.mon > dataInformada.mon
+      meses = (12 - nasc.mon) + dataInformada.mon
+    else
+      meses = dataInformada.mon - nasc.mon
+    end
+  else
+    dias = dataInformada.mday - nasc.mday
+    if nasc.mon > dataInformada.mon
+      meses = (12 - nasc.mon) + dataInformada.mon + 1
+    else
+      meses = dataInformada.mon - nasc.mon + 1
+    end
+  end
+
+  puts nasc.strftime(formatDate)
+  puts dataInformada.strftime(formatDate)
+  puts "#{anos} anos, #{meses} meses e #{dias} dias desde o nascumento"
 
 end
 
 #Entrada de dados
 
-diaN = 23
-mesN = 01
-anoN = 1981
+diaN = '23'
+mesN = '01'
+anoN = '1981'
 
-diaI = 23
-mesI = 01
-anoI = 2023
+diaI = '02'
+mesI = '09'
+anoI = '2023'
 
-dataNascimento = diaN.to_s + '/' + mesN.to_s + '/' + anoN.to_s
-dataInserida = diaI.to_s + '/' + mesI.to_s + '/' + anoI.to_s
+dataNascimento = diaN.to_i.to_s + '/' + mesN.to_i.to_s + '/' + anoN.to_i.to_s
+dataInserida = diaI.to_i.to_s + '/' + mesI.to_i.to_s + '/' + anoI.to_i.to_s
 
 findAge(dataNascimento, dataInserida)
