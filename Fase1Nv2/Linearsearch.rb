cc40b2a693cb287352b477ff8ddc27de466fd9c8
TimMarcelino <valentim.marcelino@live.com>
2023-09-04 16:28:29 -0300

    Busca linear em Array

--- Fase1Nv2/Linearsearch.rb
+++ Fase1Nv2/Linearsearch.rb
@@ -1,9 +1,21 @@
 def search(arr, valor)
   i = 0
+  located = false
   for element in arr
     i += 1
     if element == valor
+      located = true
       return i
     end
   end
+  if located == false
+    return -1
+  end
 end
+
+#Implementação e teste:
+
+array = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
+findValor = 23
+
+puts " \n\n O elemento procurado está na posição: #{search(array, findValor)}\n\n"
