390beafe94e038b1ec8ce541f247a897649abd9c
TimMarcelino <valentim.marcelino@live.com>
2023-09-05 17:14:57 -0300

    CoutingSort Resolvido

--- Fase1Nv2/couting_sort.rb
+++ Fase1Nv2/couting_sort.rb
@@ -2,30 +2,29 @@
 #não há repetição de elementos no array que vamos ordenar;
 
 def coutingSort(array)
+  n = array.length
   k = array.max # Maior valor do array
 
-  ctArray = Array.new(k+ 1)   #Array auxiliar
-  for item in array
-    ctArray[item] += 1
+  ctArray = Array.new(k, 0)   #Array auxiliar com zeros
+
+  array.each { |i| ctArray[i-1] += 1 }  # Frequencia de cada elemento
+
+  (1...k).each {|i| ctArray[i] += ctArray[i-1]}  #Atualização para posição correta
+
+  output = Array.new(n)
+
+  (n-1).downto(0) do |i|  # Inserir os elementos ordenados em Output
+    output[ctArray[array[i]-1]-1]= array[i]
+    ctArray[array[i]-1] -= 1
   end
 
-  for i in 1..(ctArray.length - 1)
-    ctArray[i] += ctArray[i - 1]
-  end
+  return output
 
-  resposta = Array.new(array.length)
-
-  for itemn in array do
-    resposta[ctArray[item]-1] = item
-    ctArray[item] -= 1
-  end
-
-  return resposta
 end
 
 ### Testes e implementações
 
-array = [6, 3, 1, 7, 2, 4, 8, 5, 0]
+array = [1, 9, 1, 3, 4, 7, 6, 7]
 #10.times{
 #  rnd = Random.new
 #  array << (rnd.rand(-100..100)).to_i
