ae2a97e533ffc2402dada019f76bab8a59326d75
Timmarcelino <valentim.marcelino@live.com>
2023-09-09 12:51:52 -0300

    Start maxEqualSum

---
+++ Fase1Nv2/maxEqualSum.rb
@@ -0,0 +1,10 @@
+=begin
+  Dadas três pilhas de números positivos, a tarefa é encontrar a possível soma máxima
+  igual das pilhas com a remoção dos elementos superiores permitida. As pilhas são
+  representadas como uma matriz e o primeiro índice da matriz representa o elemento
+  superior da pilha.
+=end
+
+def maxEqualSum(stack1, stack2, stack3)
+
+end
