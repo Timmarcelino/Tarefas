e7d5223bc3949d4c69b517d680497f2227278ffb
Timmarcelino <valentim.marcelino@live.com>
2023-09-09 16:24:42 -0300

    maxEqualSum finish

--- Fase1Nv2/maxEqualSum.rb
+++ Fase1Nv2/maxEqualSum.rb
@@ -4,58 +4,52 @@
   representadas como uma matriz e o primeiro índice da matriz representa o elemento
   superior da pilha.
 =end
-
+require 'set'
 def maxEqualSum(stack1, stack2, stack3)
+  n1 = stack1.length
+  n2 = stack2.length
+  n3 = stack3.length
 
-  stacks = [stack1, stack2, stack3]
-  stacks.each do |stack|   #Realizo a ordenação e inverto para que as primeiras posições tenham os maiores valores.
-    stack.sort!.reverse!
+  suffix = [0] * (n3 + 1)
+  st1 = Set.new
+  st2 = Set.new
+
+  sum1 = 0
+  (n1 - 1).downto(0) do |i|
+    sum1 += stack1[i]
+    st1.add(sum1)
   end
 
-  sum1 = stack1.sum
-  sum2 = stack2.sum
-  sum3 = stack3.sum
+  #puts st1.inspect
 
-  puts "S1: #{sum1}, S2: #{sum2}, S3: #{sum3}"
-
-  if sum1 == sum2 && sum2 == sum3 && sum3 == 0
-    return 0
+  sum2 = 0
+  (n2 -1).downto(0) do |i|
+    sum2 += stack2[i]
+    st2.add(sum2)
   end
 
-  loop = true
+ # puts st2.inspect
 
-  while(loop) do
-    if sum1 == sum2 && sum2 == sum3
-      loop = false
-      return sum1
-    elsif sum1 > sum2 && sum1 > sum3
-      stack1.delete_at(0)
-      sum1 = stack1.sum
-      puts "Stack1 : #{stack1.inspect}"
-    elsif sum2 > sum1 && sum2 > sum3
-      stack2.delete_at(0)
-      sum2 = stack2.sum
-      puts "Stack2 : #{stack2.inspect}"
-    elsif sum3 > sum1 && sum3 > sum2
-      stack3.delete_at(0)
-      sum3 = stack3.sum
-      puts "Stack3 : #{stack3.inspect}"
+  (n3 - 1).downto(0) do |i|
+    suffix[i] = suffix[i + 1] + stack3[i]
+  end
+
+#  puts suffix.inspect
+
+  suffix.each do |value|
+    if st1.include?(value) && st2.include?(value)
+      return value
     end
-
-    if stack1.length == 1
-      return sum1
-    elsif stack2.length == 1
-      return sum2
-    elsif stack3.length == 1
-      return sum3
-    end
-    puts "S1: #{sum1}, S2: #{sum2}, S3: #{sum3}"
   end
+  return 0
 end
 
 
 s1 = [3, 2, 1, 1, 1]
 s2 = [4, 3, 2]
-s3 = [ 2, 5, 4, 1]
+s3 = [2, 5, 4, 1]
 
-puts maxEqualSum(s1, s2, s3)
+puts s1.inspect
+puts s2.inspect
+puts s3.inspect
+puts "A soma máxima igual dos vetores dados só pode ser: #{maxEqualSum(s1, s2, s3)}"
