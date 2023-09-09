e6455d67e59472aa8857f1bd129e5b9b065cd75a
TimMarcelino <valentim.marcelino@live.com>
2023-09-08 22:43:49 -0300

    Soma maxima apos negacao desenvolvido

--- Fase1Nv2/maximumSum.rb
+++ Fase1Nv2/maximumSum.rb
@@ -15,19 +15,31 @@ def maximizeSum(array, k)
     i += 1
   end
 
+  # Após todos os números negativos tenham sido negados, decrementa i
+  i -= 1 if i == array.length
 
+  # Se k é zero ou par, retorna a soma do array
+  return array.sum if k == 0 || k % 2 == 0
 
+  # Depois, verifica se a negação de array[i] resulta em uma soma maior
+  if i != 0 && array[i].abs >= array[i - 1].abs
+    i -= 1
+  end
 
-
+  # Agora negar array[i] para obter a soma máxima e retorna a soma
+  array[i] *= -1
+  return array.sum
 end
 
 
 #Testes e implementações:
 
-arr = [ 9, 8, 8, 5]
-k = 3
-puts maximizeSum(arr, k)
+
 
 arr = [ -2, 0, 5, -1, 2 ]
 k = 4
+puts maximizeSum(arr, k)
+
+arr = [ 9, 8, 8, 5]
+k = 3
 puts maximizeSum(arr, k)
